bcc6c4b0e90c4e5c5efe9eaa8820a006c9575fd6
asma20 <jbeli.asma20@gmail.com>
2021-02-18 13:47:43 +0100

    somme de deux variable

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int c = a + b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("la somme de "+ c);
+
+
+        }
+    }
+}
